208dc7b26f8543b2e96f744a67dac19a87b2b177
Antonio Ianzano <ianzanoan@gmail.com>
2024-10-13 01:49:14 +0200

    stash

--- src/Diana/Drivers/ConfigInterface.php
+++ src/Diana/Drivers/ConfigInterface.php
@@ -6,5 +6,6 @@ interface ConfigInterface
 {
     public function get(?string $key = null): mixed;
 
-    public function setDefault(array $default): self;
+    public function addDefault(array $default): self;
+    public function getDefault(?string $key = null): array;
 }
